2cf185ca1959e04df72c88c144975250fd8c2340
le402n43\uer <s10735185@gmail.com>
2023-12-20 16:47:40 +0800

    神秘的 不在講義裡的ui.R和server.R

---
+++ 1220last/ui.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("ploting your data"), 
+  
+  sidebarPanel(
+    fileInput(inputId="files", label=h4("Upload your data file:"), multiple=FALSE, accept="text/plain"),
+    helpText("Note: you only can upload the .txt file."),
+    
+    actionButton("action1", label="Action"),
+    radioButtons("Color", "Select the color of histogram:", choices = c("Red" = "red", "Blue" = "blue", "Green" = "green"))
+  ),
+  
+  mainPanel(
+    plotOutput("scatterPlot"),
+    downloadButton ("download", label = "Download plot", class = NULL)
+  )  
+  
+))
